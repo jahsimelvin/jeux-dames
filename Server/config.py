4f51e58de8ac9ef1072ff7d54c0ed7b4fc86268a
jahsimelvin <155954108+jahsimelvin@users.noreply.github.com>
2025-03-13 16:15:49 +0100

    fichier config connexion a la DB

---
+++ Server/config.py
@@ -0,0 +1,10 @@
+import os
+
+# Configuration de la base de données
+DB_HOST = 'localhost'
+DB_NAME = 'jeu_dames'
+DB_USER = 'root'
+DB_PASSWORD = ''
+
+
+SECRET_KEY = 'votre_clé_secrète_pour_sécuriser'
